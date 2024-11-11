fba8e2d5e7259b30c9b1cc4887307837cef5863c
nizamettin sari <nizamettin.sari@student.rocva.nl>
2024-11-11 16:55:43 +0100

    JSX en state variabelen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>Hallo Nizamettin!</h1>
-    </div>
-  );
+import './App.css';
+
+import React from 'react';
+
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      myString: ''
+    }
+    this.getNumbers();
+  }
+  
+  getNumbers() {
+    for (var i=1; i < 11; i++) {
+      this.state.myString = this.state.myString + i + " ";
+    }
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        {this.state.myString}
+      </div>
+    );
+  }
+
 }
 
 export default App;
