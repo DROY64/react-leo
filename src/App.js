b3880529eb1532e82403af6ca8625baba6c6f15f
nizamettin sari <nizamettin.sari@student.rocva.nl>
2024-11-11 17:26:52 +0100

    State variabelen gebruiken

--- src/App.js
+++ src/App.js
@@ -7,25 +7,45 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      myString: ''
-    }
+      myString: '',
+      myColor: 'red',
+      addStar: 0
+    };
     this.getNumbers();
   }
   
   getNumbers() {
-    for (var i=1; i < 11; i++) {
-      this.state.myString = this.state.myString + i + " ";
+    for (var i = 0; i < 10; i++) {
+      this.state.myString = this.state.myString + "*";
     }
   }
-  
+
+  changeColor() {
+    if (this.state.myColor === "red") {
+      this.setState({ myColor: 'green' });
+    } else {
+      this.setState({ myColor: 'red' });
+    }
+  }
+
+  addStar() {
+    this.setState(prevState => ({
+      starsCount: prevState.starsCount + 1,
+      myString: prevState.myString + '*' 
+    }));
+  }
+
   render() {
     return (
-      <div className="App">
+      <div className={"App " + this.state.myColor}>
         {this.state.myString}
+        <br />
+        <button onClick={() => this.changeColor()}>Change Color</button>
+        <br />
+        <button onClick={() => this.addStar()}>Add Star</button>
       </div>
     );
   }
-
 }
 
 export default App;
