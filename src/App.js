371891251abbe91716fd9b2301268cf363048732
nizamettin sari <nizamettin.sari@student.rocva.nl>
2024-11-28 11:16:43 +0100

    REST API - deel 1 & 2

--- src/App.js
+++ src/App.js
@@ -1,117 +1,81 @@
 import './App.css';
 import React from 'react';
 
-const questions = [
-  {
-    questionText: 'Wat is de hoofdstad van Frankrijk?',
-    answerOptions: [
-      { answerText: 'New York', isCorrect: false },
-      { answerText: 'London', isCorrect: false },
-      { answerText: 'Paris', isCorrect: true },
-      { answerText: 'Dublin', isCorrect: false },
-    ],
-  },
-  {
-    questionText: 'Wie is de oprichter van Tesla?',
-    answerOptions: [
-      { answerText: 'Jeff Bezos', isCorrect: false },
-      { answerText: 'Elon Musk', isCorrect: true },
-      { answerText: 'Bill Gates', isCorrect: false },
-      { answerText: 'Tony Stark', isCorrect: false },
-    ],
-  },
-  {
-    questionText: 'Welk bedrijf was eind jaren 90 bijna failliet?',
-    answerOptions: [
-      { answerText: 'Apple', isCorrect: true },
-      { answerText: 'Intel', isCorrect: false },
-      { answerText: 'Amazon', isCorrect: false },
-      { answerText: 'Microsoft', isCorrect: false },
-    ],
-  },
-  {
-    questionText: 'Hoeveel Harry Potter boeken zijn er?',
-    answerOptions: [
-      { answerText: '1', isCorrect: false },
-      { answerText: '4', isCorrect: false },
-      { answerText: '6', isCorrect: false },
-      { answerText: '7', isCorrect: true },
-    ],
-  },
-  {
-    questionText: 'Wat is GEEN Cryptomunt?',
-    answerOptions: [
-      { answerText: 'Bitcoin', isCorrect: false },
-      { answerText: 'EOS', isCorrect: false },
-      { answerText: 'Pancake Swap', isCorrect: false },
-      { answerText: 'XOTA-XL', isCorrect: true },
-    ],
-  },
-];
-
 class App extends React.Component {
+  componentDidMount() {
+    this.getData();
+  }
+  
+  getData() {
+    fetch('http://localhost:8080/quiz/api-get')
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({
+        jsonLoaded: true,
+        jsonData: data
+      });
+    })
+    .catch(console.log);
+  }
+
   constructor(props) {
     super(props);
     this.state = {
       vraagNr: 0,
       score: 0,
-    };
-  }
-
-  handleAnswerOptionClick(isCorrect) {
-    console.log("Button clicked");
-    if (isCorrect) {
-      this.setState({ 'score': this.state.score + 1 });
-      console.log("Antwoord is goed!");
-    } else {
-      console.log("Antwoord is fout!");
+      stop: false,
     }
-    console.log("Aantal vragen: " + questions.length);
- 
+
+  }
   
-    if (this.state.vraagNr < questions.length - 1) {
-      this.setState({ vraagNr: this.state.vraagNr + 1 });
+  handleAnswerOptionClick(isCorrect) {
+    if (isCorrect) {
+      this.setState( {'score': this.state.score+1} );
+    }else{
     }
-  else {
-    this.setState( { 'stop': true } );
+    if ( this.state.vraagNr < this.state.jsonData.length-1) {
+      this.setState( {'vraagNr': this.state.vraagNr+1} );
+    } else {
+      this.setState( { 'stop': true } );
+    }
+    
   }
-
-  }
-
+  
   render() {
-    var antwoorden = questions[this.state.vraagNr].answerOptions;
-    console.log(this.state.vraagNr, questions.length);
+    if ( ! this.state.jsonLoaded ) {
+      return (  <div className="app">loading...</div>)
+    }
+    var questions=this.state.jsonData;
+    var antwoorden=questions[this.state.vraagNr].answerOptions;
+  
     return (
       <div className="app">
-        {this.state.stop ?  // hier testen we of we moeten stoppen, als we moeten stoppen laat dan het einde zien.
+        { this.state.stop  ?  
           (
             <div className='score'>
-            Jouw score is {this.state.score} van de {questions.length}
-            </div>
+  Jouw score is {this.state.score} van de {questions.length}
+</div>
           )
-          : // dit is de else, we stoppen dus niet en laten de volgende vraag zien
-          (
+          : 
+          ( 
             <>
               <div className="column">
                 <span>Vraag {this.state.vraagNr + 1} van {questions.length}</span>
                 <span className="question">{questions[this.state.vraagNr].questionText}</span>
               </div>
               
-
               <div className="column uitlijnen">
-                {antwoorden.map((antwoord) => (
+                { antwoorden.map((antwoord) => (
                   <button onClick={() => this.handleAnswerOptionClick(antwoord.isCorrect)}>{antwoord.answerText} {antwoord.isCorrect}</button>
                 ))}
-                
               </div>
-
             </>
-            
           )
         }
       </div>
-      
     );
   }
+
 }
+
 export default App;
