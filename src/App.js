55fcae4dc909d36ea53245b0c228b697ad5792d9
Leonard Ampoe <leonard.ampoe@student.rocva.nl>
2025-01-17 03:38:20 +0100

    v4

--- src/App.js
+++ src/App.js
@@ -3,28 +3,44 @@ import './App.css';
 import React from 'react';
 
 class App extends React.Component {
-  
+
   constructor(props) {
     super(props);
     this.state = {
-      myString: ''
+      myString: '',
+      myColor: 'red'
     }
     this.getNumbers();
   }
-  
+
   getNumbers() {
-    for (var i=1; i < 11; i++) {
-      this.state.myString = this.state.myString + i + " ";
+    for (var i = 0; i < 10; i++) {
+      this.state.myString = this.state.myString + "*";
     }
   }
-  
+
+
+
   render() {
     return (
-      <div className="App">
+      <div className={"App " + this.state.myColor}>
         {this.state.myString}
-        <p>Leonard</p>
+        <br />
+        <button onClick={() => this.changeColor()} >Change Color</button>
+        <button onClick={() => this.addStar()} >Add Star</button>
       </div>
     );
+  }
+  changeColor() {
+    if (this.state.myColor === "red") {
+      this.setState({ 'myColor': 'green' });
+    } else {
+      this.setState({ 'myColor': 'red' });
+    }
+  }
+
+  addStar() {
+    this.setState({ 'myString' : this.state.myString + "*"});
   }
 
 }
