5cf58116c00710e5767120784ece59d208dcb72a
Leonard Ampoe <leonard.ampoe@student.rocva.nl>
2025-01-17 02:59:46 +0100

    v3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>Goedendag Leonard!</h1>
-    </div>
-  );
+import './App.css';
+
+import React from 'react';
+
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      myString: ''
+    }
+    this.getNumbers();
+  }
+  
+  getNumbers() {
+    for (var i=1; i < 11; i++) {
+      this.state.myString = this.state.myString + i + " ";
+    }
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        {this.state.myString}
+        <p>Leonard</p>
+      </div>
+    );
+  }
+
 }
 
 export default App;
